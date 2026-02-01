03a521220b9396c84d46ca507824949c575768c9
elkin22 <elkinjoshuadelgadolopez68@gmail.com>
2026-01-31 22:26:06 -0500

    Configuración completa para Render con Cloudinary

--- build.sh
+++ build.sh
@@ -13,3 +13,14 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+
+# En buildCommand de Render
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    admin_password = os.environ.get('ADMIN_PASSWORD', 'password_temporal')
+    User.objects.create_superuser('admin', 'admin@example.com', admin_password)
+"
